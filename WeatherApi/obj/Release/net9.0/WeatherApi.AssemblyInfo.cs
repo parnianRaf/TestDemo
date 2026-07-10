9347fd82cbb9cecd9985ceacdcb5a1023a7ef3f5
parnianRaf <parnian.rafie58@gmail.com>
2026-07-10 23:34:42 +0330

    some fix

--- WeatherApi/obj/Release/net9.0/WeatherApi.AssemblyInfo.cs
+++ WeatherApi/obj/Release/net9.0/WeatherApi.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d829d0efec4f88a7948239343339d393405109")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApi")]
